1793df41ae6408d0ec94a732d2cb3a781df7e2a6
Robinsonkr <robinsonkrobert@gmail.com>
2021-01-12 18:28:04 +0530

    initial commit

---
+++ factorial.py
@@ -0,0 +1,17 @@
+#factorial
+
+number = int(input("Enter a number"))
+
+factorial = 1
+
+if number < 0:
+	print("factorial doesn't exist for negative numbers")
+
+elif number == 0:
+	print("factorial of 0 is 1")
+
+else:
+	for i in range(1,number+1):
+		factorial = factorial * i
+
+	print("factorial of ",number,"is",factorial)
